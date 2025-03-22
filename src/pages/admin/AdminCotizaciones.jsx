3af8b746ac284f0134f196eb08fa91a1ca41e7f9
Clynova <clynova.dev@gmail.com>
2025-03-21 23:35:21 -0300

    test

---
+++ src/pages/admin/AdminCotizaciones.jsx
@@ -0,0 +1,12 @@
+import { getAllQuotations } from "../../services/quotationService";
+
+const AdminCotizaciones = () => {
+    return (
+        <div>
+            <h1>Admin Cotizaciones</h1>
+            <p>Esta es la página de administración de cotizaciones.</p>
+        </div>
+    );
+}
+
+export { AdminCotizaciones };
