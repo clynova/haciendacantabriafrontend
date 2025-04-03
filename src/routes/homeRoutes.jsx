e9070d46440c252a70ab26aeddfb60237027324d
Sergio Basaez <50474233+Wixtux@users.noreply.github.com>
2025-04-02 21:35:40 -0300

    new pages
    se crearon paginas para los productos mas vendidos , las cartegorias y todos los productos

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -3,14 +3,18 @@ import { Home } from "../pages/Home";
 import { Categorias } from "../pages/Categorias";
 import { ProductDetails2 } from "../pages/ProductDetails2";
 import { MainLayout } from "../layouts/MainLayout/MainLayout";
+import { CategoriaList } from "../pages/CategoriaList";
+import TopProducts from "../pages/TopProducts";
+import AllProducts from "../pages/AllProducts";
 
 const homeRoutes = [
   <Route key="main" path="/" element={<MainLayout />}>
     <Route index element={<Home />} />
+    <Route path="categorias" element={<CategoriaList />} />
+    <Route path="categoria/:nombre" element={<Categorias />} />
     <Route path="product/:slug" element={<ProductDetails2 />} />
-  </Route>,
-  <Route key="categories" path="/categoria/:nombre" element={<MainLayout />}>
-    <Route index element={<Categorias />} />
+    <Route path="/productos" element={<AllProducts />} />
+    <Route path="/mas-vendidos" element={<TopProducts />} />
   </Route>
 ];
 
